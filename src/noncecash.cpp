bc7d42820d4ee6a57931942c9ebad927baeca435
Quankey <aldiansosmed@gmail.com>
2026-09-03 16:15:17 +0000

    first commit12

--- src/noncecash.cpp
+++ src/noncecash.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2015-2022 The NonceCash Core developers
+// Copyright (c) 2026 The NonceCash Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
