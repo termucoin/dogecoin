b00ee6b828401fac38710e8900c262ae55e8f3ee
Sawano <aldianokto@gmail.com>
2026-09-08 18:05:06 +0700

    Update genesis block output script in chainparams

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -51,7 +51,7 @@ static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesi
 static CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
 {
     const char* pszTimestamp = "ID Today 08/Sep/2026 Aldianokto, Chaos in the Treasury, Order in the Code";
-    const CScript genesisOutputScript = CScript() << ParseHex("040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9") << OP_CHECKSIG;
+    const CScript genesisOutputScript = CScript() << ParseHex("041d2fc7b78c9d7e319c2d32b12b53178afaeaadbc1252c877992ca9e9a305fefe75a3d7c6210d876c3f97ccbf3d28c9b268c74295d7f24ac66e51dfb48cf16c4e") << OP_CHECKSIG;
     return CreateGenesisBlock(pszTimestamp, genesisOutputScript, nTime, nNonce, nBits, nVersion, genesisReward);
 }
 
